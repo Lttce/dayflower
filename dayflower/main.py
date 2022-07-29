26723646c51d075b5baab85b3a02df5e9d60ecea
Lttce <31960436+Lttce@users.noreply.github.com>
2022-07-29 17:08:58 +0900

    Update: rename method

--- dayflower/main.py
+++ dayflower/main.py
@@ -1,18 +1,18 @@
 from fastapi import FastAPI
 from psycopg2.extras import RealDictCursor
-import db.util
 
+import db.util
 
 api = FastAPI()
 
 
 @api.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def get_root():
+    return {"message": "Hello World"}
 
 
-@api.get("/users")
-async def read_users():
+@api.get("/api/v1/users/")
+async def get_users():
     with db.util.get_connection() as conn:
         with conn.cursor("all", cursor_factory=RealDictCursor) as cur:
             cur.execute("select * from users;")
